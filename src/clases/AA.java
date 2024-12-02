e0d84a0d51778249d9b6133ea411c44d099a2f05
bilbobolsoncerrado <mmoncalv@tartanga.eus>
2024-12-02 19:20:11 +0100

    creado paquete clases

--- src/clases/AA.java
+++ src/clases/AA.java
@@ -1,4 +1,4 @@
-package principal;
+package clases;
 
 public class AA {
 
---
+++ src/clases/BB.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class BB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
